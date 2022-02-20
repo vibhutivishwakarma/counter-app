cde96a413ad6d28962e8043f49f2c15a3712d48f
vibhutivishwakarma <77892181+vibhutivishwakarma@users.noreply.github.com>
2022-02-20 15:21:17 +0530

    Update TotalItems.js

--- src/components/TotalItems.js
+++ src/components/TotalItems.js
@@ -7,44 +7,48 @@ export class totalItems extends Component {
 
     this.state = {
       totalitems: 0,
-      item: [
-        {
-          nos: 0,
-        },
-        {
-          nos: 0,
-        },
-        {
-          nos: 0,
-        },
-        
-      ],
-    };    
+      item: [{nos: 0},{nos: 0},{nos: 0}],
+    };
   }
 
-  deleteItem = (index) => {
+  resetAll = () => {
+    this.setState({
+      item:[{nos: 0},{nos: 0},{nos: 0}]
+    })
+  }
 
-    this.setState((ps)=> ({      
-          item: ps.item.filter((data,i)=> {
-            return i !== index
-          })
+  resetCount = () => {
+    this.setState((ps) => ({
+      item: ps.item.map(item => ({nos:0}))
     }))
-};
+  }
 
-  resetCount = ()=>{
-
-    this.setState((ps)=>({
-      item: ps.item.map((data) => {
-        return ({nos:0})
-      })
-    }))
-
+  editItem = (index, action) => {
+    console.log(index, action)
+    this.setState((ps) =>  {
+      let newArr = [...ps.item];
+      if(action === 'add'){
+        newArr[index] = {nos: newArr[index].nos + 1}
+      }
+      else if (action === 'sub' && newArr[index].nos !== 0){
+        newArr[index] = {nos: newArr[index].nos - 1}
+      }
+      else if (action === 'remove') {
+        newArr = newArr.filter((data, i) => i !== index)
+      }
+      return ({item: newArr})
+    })
   }
 
   render() {
-
-    let getItem = ()=>{
-      return this.state.item.map((data,index) => <Items key={index} delete={this.deleteItem} index={index}/>)
+    let getItem = () => {
+      return this.state.item.map((data, index) => (
+        <Items key={index} index={index} editItem ={this.editItem} nos={data.nos} />
+      ));
+    };
+  
+    let getTotalItem = () => {
+      return this.state.item.filter(item => item.nos !==0).length;
     }
 
     return (
@@ -53,7 +57,7 @@ export class totalItems extends Component {
           <p>
             <i className="fa-solid fa-cart-shopping mx-2"></i>
             <span className="badge badge-pill badge-dark totalNumber mx-2">
-              {this.state.totalitems }
+              {getTotalItem()}
             </span>
             Items
           </p>
@@ -61,12 +65,11 @@ export class totalItems extends Component {
             <button onClick={this.resetCount} type="button" className="btn btn-success mx-2">
               <i className="fa-solid fa-rotate"></i>
             </button>
-            <button type="button" className="btn btn-info text-white mx-2">
+            <button onClick={this.resetAll} type="button" className="btn btn-info text-white mx-2">
               <i className="fa-solid fa-recycle"></i>
             </button>
           </div>
         </div>
-        {/* <Items /> */}
         {getItem()}
       </>
     );
