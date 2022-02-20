9eba1e794aa454b725b4dbe45f18d4ddabf1003a
vibhutivishwakarma <77892181+vibhutivishwakarma@users.noreply.github.com>
2022-02-20 19:47:51 +0530

    Update Item.js

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 
 export default class Items extends Component {
 
-  constructor(props) {
-    super(props)
-  }
+  // constructor(props) {
+  //   super(props)
+  // }
 
   addItem = ()=>{
      this.props.editItem(this.props.index, 'add');
@@ -22,7 +22,7 @@ export default class Items extends Component {
     return (
       <div>
         <div className="selectItem my-2">
-        <span className="badge badge-pill badge-dark selectedNumber mx-3">{this.props.nos == 0 ? 'ZERO': this.props.nos}</span>
+        <span className="badge badge-pill badge-dark selectedNumber mx-3">{this.props.nos === 0 ? 'ZERO': this.props.nos}</span>
         <button type="button" onClick={this.addItem} className="btn btn-secondary controlBtn"><i className="fa-solid fa-plus"></i></button>
         <button type="button" onClick={this.subItem} className="btn btn-info controlBtn text-white"><i className="fa-solid fa-minus"></i></button>
         <button type="button" onClick={this.removeItem} className="btn btn-danger controlBtn"><i className="fa-solid fa-trash-can"></i></button>
